76b29a627c258afcc8243dd328382c4136ae867d
rahulsharma19 <rahul.rahshar@gmail.com>
2025-05-17 16:25:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ function getRandomColor() {
 }
 //box moving with random color and size functionality.
 function movebox(){
-var left = Math.random()*300;
-var right = Math.random()*400;
-var wh = (Math.random()*200)+100;
+var left;
+var top;
+var wh;
+left = Math.random()*300;
+top = Math.random()*300;
+wh = (Math.random()*400)+100;
 document.getElementById('colorbox').style.left = left;
-document.getElementById('colorbox').style.right = right;
+document.getElementById('colorbox').style.top = top;
 document.getElementById('colorbox').style.width = wh;
 document.getElementById('colorbox').style.height = wh;
 document.getElementById('colorbox').style.display="block";
+document.getElementById("colorbox").style.position = "absolute";
 document.getElementById('colorbox').style.backgroundColor = getRandomColor();
 starttime = new Date().getTime(); //initialize click start time
 }
@@ -25,6 +29,6 @@ document.getElementById('colorbox').onclick = function(){
 document.getElementById('colorbox').style.display="none";
 var endtime = new Date().getTime(); //initialize click end time
 var click_time = (endtime-starttime)/1000; //calculate and time convert miliseconds to in seconds
-alert(click_time); //showing time in seconds on click event
+alert(click_time + ' seconds'); //showing time in seconds on click event
 movebox(); //call to move box function
 }
